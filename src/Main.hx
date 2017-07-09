0f7a080433f69c246894d74be644db475c5cb8af
Kvel2D <kvelyognaut@gmail.com>
2017-07-09 11:11:13 +0300

    change map to one without markers

--- src/Main.hx
+++ src/Main.hx
@@ -4,20 +4,19 @@ import haxegon.*;
 @:publicFields
 class Main {
 
-    static inline var screen_width = 972;
-    static inline var screen_height = 972;
+    static inline var screen_width = 1080;
+    static inline var screen_height = 1080;
 
-    var position: IntVector2 = {x: 0, y: 0};
-    var forbidden = 
-    [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [7, 0],
-    [7, 1],
-    [0, 2], [7, 2], 
-    [0, 3], 
-    [7, 5], 
-    [0, 6], [2, 6], [7, 6], 
-    [0, 7], [1, 7], [2, 7], [3, 7], [6, 7], [7, 7], 
+    var position: Array<Int> = [1, 1];
+    var allowed = [
+    [5, 0], [6, 0],
+    [0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1],
+    [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], 
+    [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], 
+    [0, 5], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [6, 5], 
+    [1, 6], [3, 6], [4, 6], [5, 5], [6, 5],
+    [4, 7], [5, 7],
     ];
-    var allowed = new Array<IntVector2>();
 
     function roll() {
         position =  allowed[Random.int(0, allowed.length - 1)]; 
@@ -26,24 +25,7 @@ class Main {
     function new() {
         Gfx.resize_screen(screen_width, screen_height);
         Gfx.load_image("map");
-        Gfx.line_thickness *= 3;
-
-        function is_allowed(x, y) {
-            for (pos in forbidden) {
-                if (pos[0] == x && pos[1] == y) {
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        for (x in 0...8) {
-            for (y in 0...8) {
-                if (is_allowed(x, y)) {
-                    allowed.push({x: x, y: y});
-                }
-            }
-        }
+        Gfx.line_thickness *= 5;
 
         roll();
     }
@@ -51,12 +33,12 @@ class Main {
     function update() {
         Gfx.draw_image(0, 0, "map");
         var width = screen_width / 8;
-        Gfx.draw_box(position.x * width, position.y * width, width, width, Col.ORANGE);
-        Text.display(75, 0, '${position.x},${position.y}', Col.ORANGE);
+        Gfx.draw_box(position[0] * width, position[1] * width, width, width, Col.RED);
+        Text.display(75, 0, '${position[0]},${position[1]}', Col.RED);
+
         GUI.text_button(0, 0, "Reroll", function() { 
             roll();
         });
-
         if (Input.just_pressed(Key.R) || Input.just_pressed(Key.SPACE)) {
             roll();
         }
